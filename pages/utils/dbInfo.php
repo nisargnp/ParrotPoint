c2c0a52aa1422b902419dc38d3ec5a8c6492a38f
Daniel Choi <danielchoi42@gmail.com>
2018-03-29 18:42:08 -0400

    added db info to gitignore

--- pages/utils/dbInfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// contains fields needed to log into the database (subject to change)
-	
-	$db_host = "localhost";
-	$db_user = "dbuser";
-	$db_password = "goodbyeWorld";
-	$db_database = "teachingapp";
-?>
