bda2753e2c2ef9eb26796787d95b7faa4deb5316
Daniel Choi <danielchoi42@gmail.com>
2018-03-31 13:04:43 -0400

    Create page to upload pdfs to the database

---
+++ network_scripts/login/ProfessorSignup.php
@@ -0,0 +1,70 @@
+<?php
+	require_once("../utils/utils.php");
+
+	$bottom_text = "";
+	$db_connection = dbConnect(); // connect to the database
+
+	// process registration
+	if (isset($_POST["register"])) {
+		$username = trim($_POST["professor_username"]);
+		$name = trim($_POST["professor_name"]);
+		$password = password_hash($_POST["professor_password"], PASSWORD_DEFAULT);
+
+		// validate the fields 
+		if (validateFields($name, $username)) {
+			// insert the user into the table
+			$results = dbQuery("insert into users values ('$username', '$name', '$password')");
+
+			// TODO: redirect on success
+			// header("Location: ");
+		}
+	}
+
+	$body = <<<BODY
+		<h1>Create your Account</h1>
+		<form action="{$_SERVER['PHP_SELF']}" method="post">
+			Name: <input type="text" name="professor_name"><br><br>
+			Username: <input type="text" name="professor_username"><br><br>
+			Password: <input type="password" name="professor_password"><br><br>
+
+			<input type="submit" value="Register" name="register">
+		</form><br>
+		
+		<strong>$bottom_text</strong>
+BODY;
+	
+	echo renderPage("Create your Account", $body);
+
+
+	$db_connection->close(); // close connection
+
+
+	/*
+	 * Helper function to validate fields.
+	 *
+	 * returns empty true if valid
+	 * side-effect: modifies global bottom_text
+	 */
+	function validateFields($name, $username) {
+		global $bottom_text;
+		$isValid = true;
+
+		if (strlen($name) < 1 || strlen($name) > 50) {
+			$isValid = false;
+			$bottom_text .= nl2br("Name must be between 1 and 50 characters\n");
+		}
+		if (strlen($username) < 1 || strlen($username) > 20) {
+			$isValid = false;
+			$bottom_text .= nl2br("Username must be between 1 and 20 characters\n");
+		}
+		if (userExistsInDB($username)) {
+			$isValid = false;
+			$bottom_text .= nl2br("Username exists in the database\n");
+		}
+
+		return $isValid;
+	}
+
+
+
+
---
+++ network_scripts/upload/PDFUpload.php
@@ -0,0 +1,71 @@
+<?php
+	require_once("../utils/utils.php");
+
+	$body = ""; 
+	$db_connection = dbConnect();
+
+	// process upload (download pdf into storage -> upload to db -> delete from storage)
+	if (isset($_POST["upload"])) {
+		$serverUploadDirectory = 'temp_pdf_storage'; // temp storage 
+
+		$tmpFileName = $_FILES['uploaded_file']['tmp_name'];
+		$serverFileName = $serverUploadDirectory."/".$_FILES['uploaded_file']['name'];
+
+		// check the file was uploaded via POST
+		if (!is_uploaded_file($tmpFileName)) // BUG: is triggered for valid pdfs (file permissions?)
+			$body .= "File upload failed";
+		else {
+			// TODO: delete this
+			$body .= <<<BODY
+				<h3>Information about uploaded file</h3>
+				<p>
+					name: {$_FILES['uploaded_file']['name']} <br>
+					tmp_name: {$_FILES['uploaded_file']['tmp_name']} <br>
+					size: {$_FILES['uploaded_file']['size']} <br>
+					type: {$_FILES['uploaded_file']['type']} <br>
+				</p>
+BODY;
+			
+			//TODO: check the validity of the file type
+			
+			// copy file from temporary location to server location
+			if (!move_uploaded_file($tmpFileName, $serverFileName))
+				$body .= "File upload failed"; // file upload failure
+			else {
+				$body .= "File upload complete"; // file upload success
+
+				// get necessary fields
+				$filename = $_FILES['uploaded_file']['name'];
+				$author = ""; // TODO: fill this with the proper value (session -> professor will be logged in)
+				$pdf = $db_connection->real_escape_string(file_get_contents($serverFileName));
+
+				// insert the pdf into the database
+				if (strlen($filename) > 0 && strlen($pdf) > 0) { // TESTING: could modify/remove this check
+					$results = dbQuery("insert into pdfs (filename, author, pdf) values ('$filename', '$author', '$pdf')");
+				}
+
+				// delete the file from temporary storage
+				// POSSIBLE: move this before the query to delete the file in case the query fails
+				unlink($serverFileName);
+			}
+
+		}
+
+		// unset temp name -- not sure if this does anything
+		unset($tmpFileName);
+		unset($serverFileName);
+	}
+	else {
+		$body = <<<BODY
+		<form action="{$_SERVER['PHP_SELF']}" enctype="multipart/form-data" method="post">
+			<strong>Select a PDF to upload: </strong><br>
+			<input type="file" name="uploaded_file"><br><br>
+
+			<input type="submit" value="Upload" name="upload">
+	    </form>
+BODY;
+	}
+
+	echo renderPage("PDF Upload", $body);
+?>
+
