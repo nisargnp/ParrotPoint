9b2fa414744dc7d6402bcef787344a3097a670f4
Daniel Choi <danielchoi42@gmail.com>
2018-04-03 14:44:03 -0400

    write download pdf + change table information

---
+++ pages/upload/PDFDownload.php
@@ -0,0 +1,9 @@
+<?php
+	require_once("../utils/utils.php");
+
+	$db_connection = dbConnect();
+	//testing code to generate pdf
+	$query = "select * from pdfs where filename='spec.pdf'";
+	$results = $db_connection->query($query);
+	$record = $results->fetch_array(MYSQLI_ASSOC);
+	file_put_contents('temp_pdf_storage/filename.pdf', $record["pdf"]);
