11b27c0ce653041ec2853c752a80019d962d97f4
user.email <nnp1997@gmail.com>
2018-04-27 09:17:27 -0400

    made some changes to uploadpdf: fixed move file error, added exception handeling for query

--- pages/upload/PDFUpload.php
+++ pages/upload/PDFUpload.php
@@ -3,71 +3,72 @@
 	require_once("../utils/utils.php");
 	session_start();
 
-	$body = ""; 
-	$db_connection = dbConnect();
+	$body = "";
 
 	// process upload (download pdf into storage -> upload to db -> delete from storage)
 	if (isset($_POST["upload"])) {
-		$serverUploadDirectory = 'temp_pdf_storage'; // temp storage 
+
+		// changing some stuff up
+		// don't think we need to move the file before adding it to db
+		// can add it straight to db from tmp storage
 
 		$tmpFileName = $_FILES['uploaded_file']['tmp_name'];
-		$serverFileName = $serverUploadDirectory."/".$_FILES['uploaded_file']['name'];
 
 		// check the file was uploaded via POST
-		if (!is_uploaded_file($tmpFileName)) // BUG: is triggered for valid pdfs (file permissions?)
-			$body .= "File upload failed";
-		else {
-			// TODO: delete this
-			$body .= <<<BODY
-				<h3>Information about uploaded file</h3>
-				<p>
-					name: {$_FILES['uploaded_file']['name']} <br>
-					tmp_name: {$_FILES['uploaded_file']['tmp_name']} <br>
-					size: {$_FILES['uploaded_file']['size']} <br>
-					type: {$_FILES['uploaded_file']['type']} <br>
-				</p>
-BODY;
+		if (!file_exists($tmpFileName) || !is_uploaded_file($tmpFileName)) { // BUG: is triggered for valid pdfs (file permissions?)
 			
-			//TODO: check the validity of the file type
-			
-			// copy file from temporary location to server location
-			if (!move_uploaded_file($tmpFileName, $serverFileName))
-				$body .= "File upload failed"; // file upload failure
-			else {
-				$body .= "File upload complete"; // file upload success
+			$body .= "ERROR: FILE NOT UPLOADED";
 
-				// get necessary fields
-				$filename = $_FILES['uploaded_file']['name'];
-				$uploader = ""; // TODO: fill this with the proper value (session[professor] will be logged in)
-				$pdf = $db_connection->real_escape_string(file_get_contents($serverFileName));
-				$code = substr(md5(microtime()),rand(0,26),5);
+		} else {
+			
+			$db_connection = dbConnect();
+
+			//TODO: check the validity of the file type
+
+			// get necessary fields
+			$filename = $db_connection->escape_string($_FILES['uploaded_file']['name']);
+			$uploader = ""; // TODO: fill this with the proper value (session[professor] will be logged in)
+			$pdf = $db_connection->escape_string(file_get_contents($tmpFileName));
+			$code = substr(md5(microtime()),rand(0,26),5);
+
+			try {
 
 				// insert the pdf into the database
 				if (strlen($filename) > 0 && strlen($pdf) > 0) { // TESTING: could modify/remove this check
 					$results = dbQuery("insert into pdfs (filename, uploader, pdf, code) values ('$filename', '$uploader', '$pdf', '$code')");
 				}
 
+				$body .= "File upload complete"; // file upload success
+
+			} catch (Exception $e) {
+
+				$errmsg = $e->getMessage();
+				$body .= "Error $errmsg"; // file upload success
+
+			} finally {
+
 				// delete the file from temporary storage
-				// POSSIBLE: move this before the query to delete the file in case the query fails
-				unlink($serverFileName);
+				// will delete the file even if query fails
+				unlink($_FILES['uploaded_file']['tmp_name']);
+
 			}
 
 		}
 
 		// unset temp name -- not sure if this does anything
 		unset($tmpFileName);
-		unset($serverFileName);
 	}
-	else {
-		$body = <<<BODY
-		<form action="{$_SERVER['PHP_SELF']}" enctype="multipart/form-data" method="post">
-			<strong>Select a PDF to upload: </strong><br>
-			<input type="file" name="uploaded_file"><br><br>
 
-			<input type="submit" value="Upload" name="upload">
-	    </form>
-BODY;
-	}
+	$form = <<<HTML
+	<form action="{$_SERVER['PHP_SELF']}" enctype="multipart/form-data" method="post">
+		<strong>Select a PDF to upload: </strong><br>
+		<input type="file" name="uploaded_file"><br><br>
+
+		<input type="submit" value="Upload" name="upload">
+	</form>
+	<br><br>
+HTML;
+	$body = $form . $body;
 
 	echo renderPage("PDF Upload", $body);
 ?>
