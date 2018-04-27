6f09755961d585938145c7994a68d8e67e200200
user.email <nnp1997@gmail.com>
2018-04-27 09:59:45 -0400

    updated PDFDownload to serve pdfs directly from db

--- pages/upload/PDFDownload.php
+++ pages/upload/PDFDownload.php
@@ -1,9 +1,41 @@
 <?php
 	require_once("../utils/utils.php");
 
-	$db_connection = dbConnect();
-	//testing code to generate pdf - downloads into temp folder on apache server
-	$query = "select * from pdfs where filename='spec.pdf'";
-	$results = $db_connection->query($query);
-	$record = $results->fetch_array(MYSQLI_ASSOC);
-	file_put_contents('temp_pdf_storage/filename.pdf', $record["pdf"]);
+	// "uploader" and "filename" must be given
+	// will download file if "download" is set
+
+	if (isset($_GET["uploader"]) && isset($_GET["filename"])) {
+
+		$db_connection = dbConnect();
+		
+		$uploader = $db_connection->escape_string($_GET["uploader"]);
+        $filename = $db_connection->escape_string($_GET["filename"]);
+
+		$query = "select * from pdfs where uploader='$uploader' and filename='$filename'";
+		$result = $db_connection->query($query);
+		
+		if ($result && $result->num_rows > 0) {
+
+			$record = $result->fetch_array(MYSQLI_ASSOC);
+
+			header("Content-type: application/pdf");
+		
+			if (isset($_GET["download"])) {
+				header("Content-Disposition: attachment; filename='$filename'");
+			} else {
+				header("Content-Disposition: inline");
+			}
+			
+			echo $record["pdf"];
+		
+		} else {
+			
+			http_response_code(400);
+
+		}
+
+    } else {
+
+		http_response_code(400);		
+
+	}
