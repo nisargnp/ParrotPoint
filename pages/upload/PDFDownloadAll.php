d9a64e245d77e104e32d002057aaf2014ae34e0e
user.email <nnp1997@gmail.com>
2018-04-27 11:08:47 -0400

    getting avalible professors from db

--- pages/upload/PDFDownloadAll.php
+++ pages/upload/PDFDownloadAll.php
@@ -1,3 +1,33 @@
 <?php
 
-    echo "Hello";
+    print_r($_GET);
+
+    require_once("../utils/utils.php");
+
+    $db_connection = dbConnect();
+
+    $query = "select uploader from pdfs";
+    $result = $db_connection->query($query);
+
+    $professors = array();
+    while($row = $result->fetch_array(MYSQLI_ASSOC)) { 
+        $professors[] = $row["uploader"];
+    }
+    natcasesort($professors);
+    $professors = array_unique($professors);
+
+    $professor_options = "";
+    foreach ($professors as $professor) {
+        $professor_options .= "<option value='$professor'>$professor</option>";
+    }
+
+    $form = <<<HTML
+        <form action="{$_SERVER['PHP_SELF']}" method="get">
+            <select name="professor">
+                $professor_options
+            </select>
+            <input type="submit" name="choose_professor" value="Choose Professor">
+        </form>
+HTML;
+
+    echo renderPage("Download PDFs", $form . $bottom);
