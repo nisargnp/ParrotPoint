2bd2295f2ad81716463c98455c7be0945eae7358
user.email <nnp1997@gmail.com>
2018-04-27 13:07:18 -0400

    added no caching to pdf serve endpoint

--- pages/upload/PDFDownload.php
+++ pages/upload/PDFDownload.php
@@ -18,6 +18,11 @@
 
 			$record = $result->fetch_array(MYSQLI_ASSOC);
 
+			// no caching
+			header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+			header("Pragma: no-cache"); // HTTP 1.0.
+			header("Expires: 0"); // Proxies.
+
 			header("Content-type: application/pdf");
 		
 			if (isset($_GET["download"])) {
